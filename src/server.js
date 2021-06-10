b49927a348d02f2294620f764c3e9324615e4388
Ahmad Jailani <ahmadjailani604@gmail.com>
2021-06-10 17:25:20 +0700

    Add dotenv to setup env

--- src/server.js
+++ src/server.js
@@ -1,22 +1,20 @@
 const Hapi = require('@hapi/hapi');
 const mongoDb = require('hapi-mongodb');
+const dotenv = require('dotenv');
 const routes = require('./routes');
 
 const init = async () => {
+  dotenv.config();
+
   const server = Hapi.server({
     port: 5000,
     host: 'localhost',
-    routes: {
-      cors: {
-        origin: ['*'],
-      },
-    },
   });
 
   await server.register({
     plugin: mongoDb,
     options: {
-      url: 'mongodb://zayvich:zayvich123454321@cluster0-shard-00-00.srqlm.mongodb.net:27017,cluster0-shard-00-01.srqlm.mongodb.net:27017,cluster0-shard-00-02.srqlm.mongodb.net:27017/sample_mflix?ssl=true&replicaSet=atlas-zuhjfr-shard-0&authSource=admin&retryWrites=true&w=majority',
+      url: process.env.MONGODB_URL,
       settings: {
         useUnifiedTopology: true,
       },
