13f454b09eea655598afd2767b7e527f69711227
Ahmad Jailani <ahmadjailani604@gmail.com>
2021-06-11 17:34:15 +0700

    Update port, host, and Procfile

--- src/server.js
+++ src/server.js
@@ -7,7 +7,8 @@ const init = async () => {
   dotenv.config();
 
   const server = Hapi.server({
-    port: process.env.PORT || 5000,
+    port: process.env.PORT || 80,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
